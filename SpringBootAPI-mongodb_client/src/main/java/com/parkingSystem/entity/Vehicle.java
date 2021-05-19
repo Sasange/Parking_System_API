5bd5204ce6310ba6dea6d852ca059f1ddb215497
Sasange <asangesakshi@gmail.com>
2021-05-19 09:44:39 +0530

    Update api implemented

---
+++ SpringBootAPI-mongodb_client/src/main/java/com/parkingSystem/entity/Vehicle.java
@@ -0,0 +1,45 @@
+package com.parkingSystem.entity;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.stereotype.Component;
+import org.springframework.data.annotation.Id;
+
+@Component
+@Document(collection = "vehicle")
+public class Vehicle {
+
+	@Id
+	String id;
+	String registrationNumber;
+	String color;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getRegistrationNumber() {
+		return registrationNumber;
+	}
+
+	public void setRegistrationNumber(String registrationNumber) {
+		this.registrationNumber = registrationNumber;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	@Override
+	public String toString() {
+		return "Vehicle [Id=" + id + ", registrationNumber=" + registrationNumber + ", color=" + color + "]";
+	}
+
+}
